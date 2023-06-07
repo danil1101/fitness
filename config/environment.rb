c9a0d96b8fb691c2cf116849e012f6c70139fefe
Alex Collin <alex@collin.su>
2023-06-07 11:44:32 +0300

    Update env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.configure do
+
+    config.hosts << "starfit.store"
+
+end
 # Initialize the Rails application.
 Rails.application.initialize!
