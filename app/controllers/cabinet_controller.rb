9a43a2fd5c771648ce105c8bb933be3320fb0102
Alex Collin <alex@collin.su>
2023-06-07 20:20:03 +0300

    Redirect on buy

--- app/controllers/cabinet_controller.rb
+++ app/controllers/cabinet_controller.rb
@@ -1,7 +1,7 @@
 class CabinetController < ApplicationController
   before_action :require_user_logged_in!
   def index
-
+    @page_title = "Кабинет"
   end
 
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,12 @@
 class MainController < ApplicationController
   def index
     @user = User.new
+    @page_title = "Главная"
   end
 
   def courses
     @user = User.new
+    @page_title = "Курсы"
   end
 
   def buy
